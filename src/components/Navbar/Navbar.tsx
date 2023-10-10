b746576aaab7b4d05d641230d8c15aa61ccae51b
Joe Corrales <corralesjoe07@gmail.com>
2023-10-10 12:32:59 -0600

    rollback

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -75,7 +75,7 @@ function Navbar() {
   const goTo = ({ path }: { path: string }) => {
     console.log(`path: ${path}`);
     navigate(path);
-    navigate(0);
+    // navigate(0);
   };
 
   return (
